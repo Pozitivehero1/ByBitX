15251e01c3ee6bf4120bcd77a3d47cb77294a6e1
Pozitivehero1 <pozitivehero@gmail.com>
2026-07-27 14:39:58 +0300

    Update market.py

--- exchange/market.py
+++ exchange/market.py
@@ -2,7 +2,7 @@ import asyncio
 
 import pandas as pd
 
-from exchange.bybit_client import BybitClient
+from exchange.binance_client import BinanceClient
 
 from exchange.cache import CandleCache
 
@@ -16,7 +16,7 @@ class MarketLoader:
 
     def __init__(self):
 
-        self.client = BybitClient()
+        self.client = BinanceClient()
 
         self.cache = CandleCache()
 
